67e1e75075df955e4dde999e6e74f67556740b29
Chris Jackson <chris.scott.jackson@gmail.com>
2020-11-07 15:36:30 -0800

    fix footer on mobile (again :cry:)

--- src/Components/Footer.svelte
+++ src/Components/Footer.svelte
@@ -3,28 +3,35 @@
 </script>
 
 <style>
-  div {
+  .wrapper {
     position: fixed;
     padding: 10px;
     bottom: 0;
+    left: 0;
+    right: 0;
     background-color: var(--background-color);
-    margin: auto;
+  }
+
+  .content {
     width: clamp(20ch, 75%, 80ch);
+    margin: auto;
   }
 </style>
 
-<div>
-  © Chris Jackson
-  {currentYear}
-  -
-  <a
-    href="https://github.com/chrsjxn/chrsjxn-io"
-    target="_blank"
-    rel="noopener noreferrer">src</a>
-  -
-  <a
-    href="https://twitter.com/c_jackson_js"
-    target="_blank"
-    rel="noopener noreferrer">twitter</a>
-  - Be kind!
+<div class="wrapper">
+  <div class="content">
+    © Chris Jackson
+    {currentYear}
+    -
+    <a
+      href="https://github.com/chrsjxn/chrsjxn-io"
+      target="_blank"
+      rel="noopener noreferrer">src</a>
+    -
+    <a
+      href="https://twitter.com/c_jackson_js"
+      target="_blank"
+      rel="noopener noreferrer">twitter</a>
+    - Be kind!
+  </div>
 </div>
