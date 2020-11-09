8299306061e26d653f169354102bbe552f88ae1f
Chris Jackson <chris.scott.jackson@gmail.com>
2020-11-08 20:39:51 -0800

    initial theme support

---
+++ src/Components/ThemePicker.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { theme, cycleTheme } from '../Stores/theme'
+</script>
+
+<style>
+  button {
+    border: none;
+    background: none;
+  }
+</style>
+
+<button on:click={cycleTheme} aria-label="Toggle theme">{$theme.icon}</button>
