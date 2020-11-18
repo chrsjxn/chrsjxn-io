8de9d420a7907eb8777166cac92dbf5b56fe6bd5
Chris Jackson <chris.scott.jackson@gmail.com>
2020-11-18 00:29:26 -0800

    move links

--- src/Routes/Home.svelte
+++ src/Routes/Home.svelte
@@ -21,9 +21,6 @@
   <h2>Code Reviews</h2>
   <ul>
     <li class="post">
-      <a href="/components/theme-store">Building a theme store with Svelte</a>
-    </li>
-    <li class="post">
       <a href="/code-review/three-tips">Three tips to be a better code reviewer
         today</a>
     </li>
@@ -31,8 +28,11 @@
       <a href="/code-review/like-a-junior">Reviewing code like a junior engineer</a>
     </li>
   </ul>
-  <h1>Component Demos</h1>
+  <h1>Web Technology</h1>
   <ul>
+    <li class="component">
+      <a href="/components/theme-store">Building a theme store with Svelte</a>
+    </li>
     <li class="component"><a href="/components/polaroid">Polaroid demo</a></li>
   </ul>
 </Layout>
