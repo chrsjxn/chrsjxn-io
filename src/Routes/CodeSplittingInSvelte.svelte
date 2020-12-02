1d475ef6ea2d57d6d4aea089e9f44fd68bd6d18f
Chris Jackson <chris.scott.jackson@gmail.com>
2020-12-01 19:39:20 -0800

    Add code splitting post

---
+++ src/Routes/CodeSplittingInSvelte.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { Layout, MarkdownPost } from '../Components'
+  import { onMount } from 'svelte'
+  import NextPostLink from '../Components/NextPostLink.svelte'
+
+  let markdown = ''
+
+  onMount(async () => {
+    markdown = (await import('../Markdown/CodeSplittingInSvelte.md')).default
+  })
+
+  export let nextPost = null
+</script>
+
+<Layout>
+  <MarkdownPost {markdown} />
+  {#if markdown}
+    <NextPostLink {nextPost} />
+  {/if}
+</Layout>
