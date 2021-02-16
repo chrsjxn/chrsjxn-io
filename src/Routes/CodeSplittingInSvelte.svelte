0a531c91a7ed93012eef17509879cc555feb3327
Chris Jackson <chris.scott.jackson@gmail.com>
2021-02-15 23:01:43 -0800

    Initial dynamic imports for posts

--- src/Routes/CodeSplittingInSvelte.svelte
+++ src/Routes/CodeSplittingInSvelte.svelte
@@ -1,20 +1,12 @@
 <script>
   import { Layout, MarkdownPost } from '../Components'
-  import { onMount } from 'svelte'
   import NextPostLink from '../Components/NextPostLink.svelte'
-
-  let markdown = ''
-
-  onMount(async () => {
-    markdown = (await import('../Markdown/CodeSplittingInSvelte.md')).default
-  })
+  import markdown from '../Markdown/CodeSplittingInSvelte.md'
 
   export let nextPost = null
 </script>
 
 <Layout>
   <MarkdownPost {markdown} />
-  {#if markdown}
-    <NextPostLink {nextPost} />
-  {/if}
+  <NextPostLink {nextPost} />
 </Layout>
